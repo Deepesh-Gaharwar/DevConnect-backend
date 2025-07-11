d4683fcfb3e2fe0d4096b2f10d03e075f19714a6
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2025-07-11 22:28:32 +0530

    wildcard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,25 +2,36 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
+app.get("/getUserData", (req,res) => {
+ 
+ // always use try-catch block 
 
-app.use("/admin",adminAuth);
+    try {
+       // logic of DB call and get User Data
+       throw new Error("dsjccbdj");
+       res.send("User Data Send");
+        
+    } catch (error) {
+        res.status(500).send("Some Error contact support team");
 
-app.post("/user/login", (req,res) => {
-    res.send("User logged in successfully")
+        // 2nd method 
+       // res.status(500).send(error.message);
+    }
+
+
+    
 });
 
-app.get("/user/data", userAuth, (req,res) => {
-    res.send("User Data Send");
+// some applications use this method of handling the errors
+
+app.use("/", (err,req,res,next) => {
+    if(err){
+
+        // log your error message 
+        res.status(500).send("Something went wrong");
+    }
 })
 
-app.get("/admin/getAllData", (req,res) => {
-    res.send("All Data Sent")
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
-});
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
