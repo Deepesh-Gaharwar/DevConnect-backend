54f70b5b9f74dc535af9963a6b4c9cc9c54ec9bf
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2025-07-11 13:28:05 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
+// this will only handle GET call to /user
+app.get("/user", (req,res) => {
+    res.send({ firstName: "Deepesh", lastName: "Gaharwar"})
+})
+
+// post call
+app.post("/user", (req,res) => {
+    // saving data to the DB
+    res.send("Data successfully saved to the Databse");
+})
+
+// delete call
+app.delete("/user", (req,res) => {
+    res.send("Deleted successfully !!!")
+})
+
 app.use("/test",(req,res) => {
     res.send("Hello from the server !!!");
 })
-app.use("/",(req,res) => {
-    res.send("Home home !!!");
-})
+
+
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
