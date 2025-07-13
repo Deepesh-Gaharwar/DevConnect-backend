815ab9031d8180467d4f513e359967519e2da8ff
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2025-07-13 22:21:05 +0530

    created login API

--- src/app.js
+++ src/app.js
@@ -1,25 +1,42 @@
 const express = require("express");
-
-
 const {connectDB} = require("./DB/db.js");
-
+const app = express();
 // requiring models
 const {User} = require("./models/user.js");
+const {validateSignUpData} = require("./utils/validation.js");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const app = express();
 
-app.use(express.json()); // to read the json data coming from the requests to body
+
+app.use(express.json()); // to read the json data coming from the request to body
 
 
 // /signup -> route
 
 app.post("/signup",async (req,res) => {
-
- 
- // creating a new instance of the user Model 
-    const user = new User (req.body);
   
     try{ 
+
+     // validation of the data
+       validateSignUpData(req);
+
+
+     //encrypt the password
+
+     const {firstName, lastName, emailId, password} = req.body;
+
+      const passwordHash = await bcrypt.hash(password,10);
+      
+
+ 
+     // creating a new instance of the user Model 
+       const user = new User ({
+         firstName,
+         lastName,
+         emailId,
+         password : passwordHash,
+       });
 
         await user.save();
 
@@ -28,12 +45,50 @@ app.post("/signup",async (req,res) => {
         console.log("User added successfully");
         
     }catch(err){
-       res.status(400).send("Error saving the user :"+ err.message) ;
+       res.status(400).send("Error :"+ err.message) ; 
     }
    
     
 
 });
+
+
+// login route handler 
+app.post("/login", async (req,res) => {
+
+    try {
+
+       const {emailId, password} = req.body;
+     
+       // is emailId valid 
+       if(!validator.isEmail(emailId)){
+       
+               throw new Error("Email is not valid!");
+       
+        }
+
+       // whether the user is in the DB , then check for the password compare i.e.,correct or not
+
+       const user = await User.findOne({emailId : emailId});
+
+       if(!user){
+         throw new Error("Invalid credentials!")
+
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(isPasswordValid){
+            res.status(200).send("Login successfull!");
+        }else{
+            throw new Error("Invalid credentials!");
+        }
+        
+    } catch (error) {
+        res.status(400).send("Error :" +error.message);
+        
+    }
+})
 
 
 // get user by email
