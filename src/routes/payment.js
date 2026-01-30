37d3d8e8a8d7e779d0c3b50e999f61d9df9abd26
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2026-01-30 18:04:16 +0530

    fixes db call in payment route

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -122,7 +122,7 @@ paymentRouter.get("/premium/verify", userAuth, async(req, res) => {
 
   try {
 
-    const user = await User.findById(req.user._id);
+    const user = await User.findOne(req.user._id);
 
     if (!user) {
       return res.status(404).json({ isPremium: false });
